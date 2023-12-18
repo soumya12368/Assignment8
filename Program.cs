8f5989cc53f5b1461009200dcdb1c32f58f7c5d9
Soumya Sutar <soumsutar9@gmail.com>
2023-12-18 15:39:48 +0530

    Assignment8

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=SOUMYA;Initial Catalog=Products;Integrated Security=True";
+        string queryString = "SELECT TOP 5 * FROM Products ORDER BY PName DESC";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("\t{0}\t{1}\t{2}\t{3}\t{4}", reader[0], reader[1], reader[2], reader[3], reader[4]);
+            }
+
+            reader.Close();
+        }
+    }
+}
